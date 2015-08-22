69147f36af812238811ec2aebdb842726c0aa793
Jørgen Sundt <jorgen@theeleven.co.uk>
2015-08-22 15:22:11 +0100

    changed file paths

--- lib/theeleven/boilerplate.rb
+++ lib/theeleven/boilerplate.rb
@@ -18,28 +18,28 @@ module Theeleven
         FileUtils.mkdir_p "app/views/shared"
 
         #Application CSS
-        copy_file "templates/css/application.scss", "app/assets/stylesheets/application.scss"
+        copy_file "../../templates/css/application.scss", "app/assets/stylesheets/application.scss"
         #Base CSS files
-        copy_file "templates/css/base/animations.scss", "app/assets/stylesheets/base/animations.scss"
-        copy_file "templates/css/base/config.scss", "app/assets/stylesheets/base/config.scss"
-        copy_file "templates/css/base/mixins.scss", "app/assets/stylesheets/base/mixins.scss"
-        copy_file "templates/css/base/shame.scss", "app/assets/stylesheets/base/shame.scss"
-        copy_file "templates/css/base/type.scss", "app/assets/stylesheets/base/type.scss"
-        copy_file "templates/css/base/layout.scss", "app/assets/stylesheets/base/layout.scss"
+        copy_file "../../templates/css/base/animations.scss", "app/assets/stylesheets/base/animations.scss"
+        copy_file "../../templates/css/base/config.scss", "app/assets/stylesheets/base/config.scss"
+        copy_file "../../templates/css/base/mixins.scss", "app/assets/stylesheets/base/mixins.scss"
+        copy_file "../../templates/css/base/shame.scss", "app/assets/stylesheets/base/shame.scss"
+        copy_file "../../templates/css/base/type.scss", "app/assets/stylesheets/base/type.scss"
+        copy_file "../../templates/css/base/layout.scss", "app/assets/stylesheets/base/layout.scss"
         #CSS Modules
-        copy_file "templates/css/modules/buttons.scss", "app/assets/stylesheets/modules/buttons.scss"
-        copy_file "templates/css/modules/icons.scss", "app/assets/stylesheets/modules/icons.scss"
+        copy_file "../../templates/css/modules/buttons.scss", "app/assets/stylesheets/modules/buttons.scss"
+        copy_file "../../templates/css/modules/icons.scss", "app/assets/stylesheets/modules/icons.scss"
         
 
         #Application Layout
-        copy_file "templates/views/layouts/application.html.erb", "app/views/layouts/application.html.erb"
+        copy_file "../../templates/views/layouts/application.html.erb", "app/views/layouts/application.html.erb"
         #Shared views
-        copy_file "templates/views/shared/_favicons.html.erb", "app/views/shared/_favicons.html.erb"
-        copy_file "templates/views/shared/_scripts.html.erb", "app/views/shared/_scripts.html.erb"
-        copy_file "templates/views/shared/_navbar.html.erb", "app/views/shared/_navbar.html.erb"
-        copy_file "templates/views/shared/_footer.html.erb", "app/views/shared/_footer.html.erb"
+        copy_file "../../templates/views/shared/_favicons.html.erb", "app/views/shared/_favicons.html.erb"
+        copy_file "../../templates/views/shared/_scripts.html.erb", "app/views/shared/_scripts.html.erb"
+        copy_file "../../templates/views/shared/_navbar.html.erb", "app/views/shared/_navbar.html.erb"
+        copy_file "../../templates/views/shared/_footer.html.erb", "app/views/shared/_footer.html.erb"
         #Icon set
-        copy_file "templates/views/icons/_sprite.html.erb", "app/views/icons/_sprite.html.erb"
+        copy_file "../../templates/views/icons/_sprite.html.erb", "app/views/icons/_sprite.html.erb"
 
       end
     end #end generator
